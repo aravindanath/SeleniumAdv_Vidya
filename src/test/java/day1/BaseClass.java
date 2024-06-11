2a2fb661ff3be0ac3f0fe5819a0abb63c871f15e
vidyanath <vidyanathnalla58@gmail.com>
2024-06-11 14:07:18 +0530

    pushing practice xpath file

--- src/test/java/day1/BaseClass.java
+++ src/test/java/day1/BaseClass.java
@@ -3,6 +3,7 @@ package day1;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
@@ -14,7 +15,16 @@ public class BaseClass {
 
     @BeforeClass
     public void setup(){
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-notifications");
+        options.addArguments("--disable-popup-blocking");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        // Add geolocation permission
+        options.addArguments("--enable-geolocation");
+
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(45));
     }
--- src/test/java/day1/redbus.java
+++ src/test/java/day1/redbus.java
@@ -1,17 +1,28 @@
 package day1;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.testng.annotations.Test;
 
 public class redbus extends BaseClass{
 
     @Test
-    public void Xpath_6() {
+    public void Xpath_6() throws InterruptedException {
 
         driver.get("https://www.redbus.in/");
-        driver.findElement(By.id("src")).sendKeys("Hyderabad");
+        driver.findElement(By.id("src")).sendKeys("Hyderabad", Keys.ENTER);
         driver.findElement(By.id("dest")).sendKeys("Vijayawada", Keys.ENTER);
+        driver.findElement(By.id("search_button")).click();
+
+        Thread.sleep(2000);
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        js.executeScript("window.scrollBy(0,500)");
+
+//        Thread.sleep(4000);
+        System.out.println("Executed");
 
 
 
